a0f2d51791e11a505e35bae18fc368d3ce2dc69a
ilhan <ilhanzengn@gmail.com>
2022-07-17 00:09:05 +0300

    commit1

--- src/C09_GitProject01.java
+++ src/C09_GitProject01.java
@@ -3,7 +3,8 @@ public class C09_GitProject01 {
     public static void main(String[] args) {
         System.out.println("I am learning GitHub");
 
-       //" hello world""
+        System.out.println(" hello world");
+
 
     }
 

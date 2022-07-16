7859a089f7a744ab23c89693d552e9912a26a4c7
ilhan <ilhanzengn@gmail.com>
2022-07-16 23:35:03 +0300

    Hello World

---
+++ src/C09_Ornek.java
@@ -0,0 +1,30 @@
+
+/*
+import java.util.stream.IntStream;
+
+public class C09_Ornek {
+
+
+
+            //1) Create a method to find the sum of integers from 7 to 100
+
+public static int getSumFromSevenToHundred01(){
+
+    return IntStream.range(7,100).reduce(0,Math::addExact);
+
+
+}
+// 2nd Way:
+
+    public static int getSumFromSevenToHundred02(){
+
+    return IntStream.rangeClosed(7,100).reduce(0,Math::addExact);
+
+
+
+}
+
+//2) Create a method to find the multiplication of the integers from 2(inc) to 11(inc)
+
+public IntStream.rangeClosed(2,11)
+*/
